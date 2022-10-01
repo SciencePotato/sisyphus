8fb5d7a3636ba8bfa5948fda74cd52c82a855ca5
deybyr647 <deybyr647@gmail.com>
2022-10-01 12:24:37 -0400

    Began adding React Context logic for accessing local storage data

---
+++ src/components/storage/util.js
@@ -0,0 +1,13 @@
+const getHabitData = () => {
+
+}
+
+const updateHabitData = () => {
+
+}
+
+const updateHabitActivities = () => {
+
+}
+
+export { getHabitData, updateHabitData, updateHabitActivities };
