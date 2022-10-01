b5a9e3984fcd0e0efe7da484c9608fee4c2a8fd3
Hou <64312205+SciencePotato@users.noreply.github.com>
2022-10-01 15:13:44 -0400

    Created Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import './App.css';
 import Loader from './page/loader/Loader.js';
 import Welcome from './page/welcome/Welcome.js';
+import TopDisplay from './components/topDisplay/TopDisplay.js'
 
 function App() {
   return (
     <div className="App">
-      <Welcome/>
+      {/* <Welcome/> */}
       {/* <Loader/> */}
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopDisplay/>
     </div>
   );
 }
