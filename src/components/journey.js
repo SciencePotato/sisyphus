88ad43280d687eb7a48a3c0b2648238d83bbd3f9
zikunwang-terry <terryiochina@gmail.com>
2022-10-01 15:10:20 -0400

    create journey.js and record-card.js

---
+++ src/components/journey.js
@@ -0,0 +1,45 @@
+import RecordCard from "./record-card";
+import './journey.css'
+
+const test_data = [
+  {
+    'title': 'task1',
+    'date': '23 Sep 2022',
+    'description': 'Today i jogged for 10 miles',
+  },
+  {
+    'title': 'task2',
+    'date': '22 Sep 2022',
+    'description': 'Today i jogged for 5 miles, kinda tired right now.',
+  },
+  {
+    'title': 'task3',
+    'date': '19 Sep 2022',
+    'description': 'I wanna start to jogging. Just for fun.',
+  }, 
+  {
+    'title': 'task3',
+    'date': '18 Sep 2022',
+    'description': 'I wanna start to jogging. Just for fun.',
+  },
+  {
+    'title': 'task3',
+    'date': '15 Sep 2022',
+    'description': 'I wanna start to jogging. Just for fun.',
+  }
+]
+
+const Journey = () => {
+  return (
+    <div className="journey">
+      <h1>Journey</h1>
+      <div className="records-list">
+        {test_data.map(({title, date, description}) => 
+          <RecordCard title={title} date={date} description={description} />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Journey;
---
+++ src/components/record-card.js
@@ -0,0 +1,20 @@
+import './record-card.css'
+
+const RecordCard = ({date, title, description}) => {
+  //Parse date
+  let day = date[0] + date[1]
+  let rest_date = date.slice(3)
+
+  return (
+    <div className="record-card">
+      <div className='record-card-date'>
+        <h1>{day}</h1>
+        <h2>{rest_date}</h2>
+      </div>
+      
+      <p>{description}</p>
+    </div>
+  )
+}
+
+export default RecordCard;
