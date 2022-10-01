4dd6844bd4d00b570c40108f3ed7b2115856ec45
Hou <64312205+SciencePotato@users.noreply.github.com>
2022-10-01 16:55:23 -0400

    Stats page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Loader from './page/loader/Loader.js';
 import Welcome from './page/welcome/Welcome.js';
 import TopDisplay from './components/topDisplay/TopDisplay.js'
 import Journey from './components/journey';
+import Stats from './page/stats/Stats';
 
 function App() {
   return (
     <div className="App">
       {/* <Welcome/> */}
       {/* <Loader/> */}
-      <TopDisplay/>
-      <Journey/>
+      {/* <TopDisplay/>
+      <Journey/> */}
+      <Stats/>
     </div>
   );
 }
---
+++ src/page/stats/Stats.js
@@ -0,0 +1,26 @@
+import './stats.css'
+import manBall from './../../images/manball_1.png'
+
+function Stats() {
+  return (
+    <>
+        <div className='stats'>
+            <div>
+                <div>
+                    <h2> You already climbed: </h2>
+                    <h1>21.208 ft</h1>
+                    <h3> On your 47th day </h3>
+                    <h2>You created <span className='stats-date'> 13 </span> posts.</h2>
+                    <p>“I leave Sisyphus at the foot of the mountain! One always finds one’s burden again. But Sisyphus teaches the higher fidelity that negates the gods and raises rocks. He too concludes that all is well. This universe henceforth without a master seems to him neither sterile nor futile. Each atom of that stone, each mineral flake of that night filled mountain, in itself forms a world. The struggle itself toward the heights is enough to fill a man’s heart. One must imagine Sisyphus happy. ”</p>
+                    <p>Albert Camus</p>
+                </div>
+            </div>
+            <div>
+                <img src={manBall}/>
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default Stats;
