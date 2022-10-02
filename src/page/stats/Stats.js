69b067be5714641f674c10218390e1cd81095c48
zikunwang-terry <terryiochina@gmail.com>
2022-10-02 12:05:00 -0400

    word change

--- src/page/stats/Stats.js
+++ src/page/stats/Stats.js
@@ -43,7 +43,7 @@ function Stats() {
                     <div>
                         <h2> You already climbed: </h2>
                         <h1>{height.toFixed(3)} ft</h1>
-                        <h3>You're on a {streak} day streak</h3>
+                        <h3>{streak} day(s) since you joined Sisyphus</h3>
                         <h2>You created <span className='stats-date'> {activities.length} </span> posts.</h2>
                         <p>“I leave Sisyphus at the foot of the mountain! One always finds one’s burden again. But Sisyphus teaches the higher fidelity that negates the gods and raises rocks. He too concludes that all is well. This universe henceforth without a master seems to him neither sterile nor futile. Each atom of that stone, each mineral flake of that night filled mountain, in itself forms a world. The struggle itself toward the heights is enough to fill a man’s heart. One must imagine Sisyphus happy. ”</p>
                         <p>Albert Camus</p>
