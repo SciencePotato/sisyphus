908a9c88a4bf96140404607aededbd8afb226324
zikunwang-terry <terryiochina@gmail.com>
2022-10-02 09:08:28 -0400

    only one entry per day

--- src/components/topDisplay/goalPoint/GoalPoint.js
+++ src/components/topDisplay/goalPoint/GoalPoint.js
@@ -1,18 +1,27 @@
 import './GoalPoint.css';
 import {useHabit} from "../../storage/HabitContext";
 import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 function GoalPoint() {
     const habitContext = useHabit();
+    const navigate = useNavigate();
+
     const [deltaHeight, setDeltaHeight] = useState(0);
+    const [height, setHeight] = useState(0);
+    const [date, setDate] = useState("");
 
     useEffect(() => {
         setDeltaHeight(habitContext.getDeltaHeight());
+        setHeight(habitContext.getProgress());
+        setDate(new Date().toDateString());
     }, [habitContext]);
   return (
     <>
         <div className='goalPoint'>
             <h3>+{deltaHeight.toFixed(3)} ft</h3>
+            <h1>{height.toFixed(3)} ft</h1>
+            <h3>{date}</h3>
         </div>
     </>
   );
